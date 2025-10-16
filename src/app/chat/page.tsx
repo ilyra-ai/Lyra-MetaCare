0907668142ee7c467080219244431248ed626c63
ilyra-ai <douglas@ilyra.com.br>
2025-10-16 15:44:12 -0300

    Updated src/app/chat/page.tsx

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -24,7 +24,7 @@ export default function ChatPage() {
       <div className="flex flex-col flex-1 min-w-0">
         <Header />
         <main className="flex-1 flex flex-col p-4 sm:p-6 md:p-8 animate-in fade-in duration-500">
-           <div className="flex-0 flex flex-col min-h-0">
+           <div className="flex-1 flex flex-col min-h-0">
             <ChatAssistantContent />
           </div>
         </main>
