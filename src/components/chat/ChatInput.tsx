d054816f1739c9714b5827f04a06670c47e85778
ilyra-ai <douglas@ilyra.com.br>
2025-10-16 16:02:44 -0300

    Updated src/components/chat/ChatInput.tsx

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -584,7 +584,6 @@ export function ChatInput({
           position: absolute;
           width: 1px;
           height: 1px;
-          padding: 0;
           margin: -1px;
           overflow: hidden;
           clip: rect(0, 0, 0, 0);
